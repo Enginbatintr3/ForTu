928c2fa8cd1444f661a48b46be89d2a3b1bfbd67
phoenixen <enginsoftwaremnr@gmail.com>
2021-07-07 12:20:05 +0300

    ForTu

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_3/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "Fortune Teller App",
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
